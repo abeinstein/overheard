8cecc5f762c9b8bceb1e2bac6a593a54321ac539
Andrew <abeinstein@uchicago.edu>
2013-12-08 16:28:01 -0600

    Heroku stuff

--- overheard/overheard/wsgi.py
+++ overheard/overheard/wsgi.py
@@ -10,5 +10,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "overheard.settings")
 
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
